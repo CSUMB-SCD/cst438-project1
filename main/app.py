495e5b685525b8289143eaf434792adee26e6c98
Nicole Hipolito <nhipolito@Nicoles-MacBook-Pro.local>
2017-11-14 19:42:59 -0800

    reverted code to base state

--- main/app.py
+++ main/app.py
@@ -1,16 +1,10 @@
-import os
 from flask import Flask, render_template
-from flask_socketio import SocketIO, send, emit
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'mysecret'
-socketio = SocketIO(app)
-
-@socketio.on('message')
-def handleMessage(msg):
-    send(msg, broadcast=True)
-    print('Message: ' + str(msg)) #error happens here.
-    
-    
+ 
+app = Flask(__name__)      
+ 
+@app.route('/')
+def home():
+  return render_template('home.html')
+ 
 if __name__ == '__main__':
-    socketio.run(app)
+  app.run()
