9a0e872c258e06a7cce52243b123b67214ae539f
Nicole Hipolito <nhipolito@Nicoles-MacBook-Pro.local>
2017-11-28 18:39:31 -0800

    fixed bugs

--- main/app.py
+++ main/app.py
@@ -32,10 +32,10 @@ def login():
 	return render_template('login.html') 
 
 if __name__ == "__main__":
-    # app.run(host='0.0.0.0', port=8080, debug=True)
-    app.run(
-    port=int(os.getenv('PORT', 8080)),
-    host=os.getenv('IP', '0.0.0.0'),
-    debug=True
-    )
+    app.run(host='0.0.0.0', port=8080, debug=True)
+    # app.run(
+    # port=int(os.getenv('PORT', 8080)),
+    # host=os.getenv('IP', '0.0.0.0'),
+    # debug=True
+    # )
 
