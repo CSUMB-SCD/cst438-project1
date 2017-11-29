b4bbb7474ce25e7386e7eecc6af5dfeae85f3488
Nicole Hipolito <nhipolito@Nicoles-MacBook-Pro.local>
2017-11-28 19:12:10 -0800

    fixed bugs

--- main/app.py
+++ main/app.py
@@ -35,8 +35,8 @@ def login():
 if __name__ == "__main__":
     # app.run(host='0.0.0.0', port=8080, debug=True)
     app.run(
-    port=int(os.environ.get('PORT')),
-    host=os.environ.get('IP'),
+    port=int(os.environ.get('PORT', None)),
+    host=os.environ.get('IP', None),
     debug=False
     )
 
